583e9344d3de8fffddd08ef15b203ca48e30dd8a
filim <filimonbogdan89@gmail.com>
2020-09-17 16:28:17 +0300

    readme

--- src/To_Do_List/Main.java
+++ src/To_Do_List/Main.java
@@ -1,3 +1,10 @@
+/*
+ O aplicatie de tip to-do list, in care avem butoane pentru a adauga task-uri (in care putem trece numele task-ului,
+ data si o scurta descriere), putem sterge task-uri prin select + remove task, putem modifica un task prin selectarea lui si
+ apasarea butonului corespunzator si putem sa le marcam ca rezolvate prin dublu click (colorare verde)
+ */
+
+
 package To_Do_List;
 
 public class Main {
